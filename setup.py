112334edd66161f4afd4a96ce678d89864a6b1aa
Jiri Kulda <jkulda@redhat.com>
2017-04-10 21:36:03 +0200

    Metamorph refactorization due bad installation
    It was needed to create metamorph folder which will contain
    implementation details.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='metamorph',
     version='0.1',
-    packages=['lib', 'etc', 'plugins', 'plugins.library'],
+    packages=['lib', 'etc', 'metamorph', 'metamorph.library', 'metamorph.plugins'],
     url='https://github.com/RHQE/metamorph',
     license='',
     author='Jiri Kulda',
