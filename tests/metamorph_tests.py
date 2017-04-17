fabff4d967080acd52514ad373c10d0ea75c1277
Jiri Kulda <jkulda@redhat.com>
2017-04-17 23:20:34 +0200

    Extracting rpm-mappings from pdc
    + added tests

--- tests/metamorph_tests.py
+++ tests/metamorph_tests.py
@@ -176,6 +176,13 @@ class MyTestCase(unittest.TestCase):
         client.setup_pdc_metadata_params("component", "version", "release")
         self.assertDictEqual(client.pdc_name_mapping, output)
 
+    def test_pdc_rpm_mappings(self):
+        client = PDCApi("", "", "bash-completion-version-release")
+        with open("./tests/sources/test_rpm_mappings.json") as rpm_mapping_input:
+            pdc_data = json.load(rpm_mapping_input)
+        output = {'rhel-7.1', 'rhel-7.0'}
+        self.assertSetEqual(client.get_release_ids(pdc_data['release-components'], pdc_data['rpms']), output)
+
 
 if __name__ == '__main__':
     unittest.main()
