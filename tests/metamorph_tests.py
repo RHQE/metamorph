7403358947d13c9ade36d29241a58a9999f86950
Jiri Kulda <jkulda@redhat.com>
2017-04-29 00:20:01 +0200

    Added tests for dispatcher plugin.
    + ansible plugin tests

--- tests/metamorph_tests.py
+++ tests/metamorph_tests.py
@@ -3,6 +3,7 @@ import json
 import os
 
 from metamorph.plugins.morph_messagehub import env_run
+from metamorph.library.message_data_extractor import MessageDataExtractor as MessageDataExtractorAnsible
 from metamorph.plugins.morph_resultsdb import ResultsDBApi
 from metamorph.plugins.morph_pdc import PDCApi
 from metamorph.library.pdc import PDCApi as PDCApiAnsible
@@ -178,6 +179,87 @@ class MyTestCase(unittest.TestCase):
         os.environ['TEST'] = json.dumps(data)
         output = env_run(SimpleClass('TEST'))
         self.assertDictEqual(output, data)
+
+    @unittest.skip("Travis CI does not have access to RH site.")
+    def test_resultdb_query(self):
+        resultsdb = ResultsDBApi("", "kernel-3.10.0-632.el7", "1", "https://url.corp.redhat.com/resultdb2", "")
+        self.assertEqual(len(resultsdb.get_resultsdb_data()), 200)
+
+    def test_data_extractor_check_fail2(self):
+        message = {'message': {"weight": 0.2, "parent": None},
+                   'header': {"owner": "jkulda",
+                              "scratch": "true",
+                              "method": "build",
+                              "target": "rhel-7.1-candidate",
+                              "new": "RUNNING",
+                              "packages": "setup",
+                              "version": "2.8.71",
+                              "release": "5.el7_1"
+                              }
+                   }
+        extractor = MessageDataExtractor(None)
+        extractor.ci_message = message
+        self.assertEqual(extractor.check_valid_ci_message(), False)
+
+    def test_data_extractor_pass_ansible(self):
+        message = {'message': {"weight": 0.2, "parent": None},
+                   'header': {"owner": "jkulda",
+                              "scratch": "true",
+                              "method": "build",
+                              "target": "rhel-7.1-candidate",
+                              "new": "CLOSED",
+                              "package": "setup",
+                              "version": "2.8.71",
+                              "release": "5.el7_1"
+                              }
+                   }
+        output = {'scratch': 'true',
+                  'version': '2.8.71',
+                  'owner': 'jkulda',
+                  'release': '5.el7_1',
+                  'package': 'setup',
+                  'target': 'rhel-7.1-candidate'}
+        extractor = MessageDataExtractorAnsible(None)
+        extractor.ci_message = message
+        self.assertEqual(extractor.check_valid_ci_message(), True)
+        self.assertEqual(extractor.get_build_data(), output)
+
+    def test_data_extractor_check_fail_ansible(self):
+        message = {'message': {"weight": 0.2, "parent": None},
+                   'header': {"owner": "jkulda",
+                              "scratch": "true",
+                              "method": "buildArch",
+                              "target": "rhel-7.1-candidate",
+                              "new": "CLOSED",
+                              "packages": "setup",
+                              "version": "2.8.71",
+                              "release": "5.el7_1"
+                              }
+                   }
+        extractor = MessageDataExtractorAnsible(None)
+        extractor.ci_message = message
+        self.assertEqual(extractor.check_valid_ci_message(), False)
+
+    def test_data_extractor_check_fail2_ansible(self):
+        message = {'message': {"weight": 0.2, "parent": None},
+                   'header': {"owner": "jkulda",
+                              "scratch": "true",
+                              "method": "build",
+                              "target": "rhel-7.1-candidate",
+                              "new": "RUNNING",
+                              "packages": "setup",
+                              "version": "2.8.71",
+                              "release": "5.el7_1"
+                              }
+                   }
+        extractor = MessageDataExtractorAnsible(None)
+        extractor.ci_message = message
+        self.assertEqual(extractor.check_valid_ci_message(), False)
+
+    @unittest.skip("Travis CI does not have access to RH site.")
+    def test_resultdb_query(self):
+        resultsdb = ResultsDBApi("", "kernel-3.10.0-632.el7", "1", "https://url.corp.redhat.com/resultdb2", "")
+        self.assertEqual(len(resultsdb.get_resultsdb_data()), 200)
 
     def test_env_message_part_with_newlines(self):
         data = '{\n"old": \n"OPEN", \n"new": \n"FAILED", \n"attribute": \n"state"}'
