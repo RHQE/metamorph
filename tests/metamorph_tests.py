b1cc647594bd16a05da7679d229a422e5d33902b
Jiri Kulda <jkulda@redhat.com>
2017-04-28 22:52:15 +0200

    Fixed problem with printing json results in messagehub plugin
    Plugin does not printed json file in pretty print format.
    Fixed by adding parsing by json package.
    
    + added tests

--- tests/metamorph_tests.py
+++ tests/metamorph_tests.py
@@ -1,9 +1,17 @@
 import unittest
 import json
+import os
 
 from metamorph.plugins.morph_message_data_extractor import MessageDataExtractor
 from metamorph.library.message_data_extractor import MessageDataExtractor as MessageDataExtractorAnsible
+from collections import namedtuple
+from metamorph.plugins.morph_messagehub import env_run
 from metamorph.plugins.morph_resultsdb import ResultsDBApi
+
+
+class SimpleClass(object):
+    def __init__(self, env_variable):
+        self.env_variable = env_variable
 
 
 class MyTestCase(unittest.TestCase):
@@ -139,6 +147,20 @@ class MyTestCase(unittest.TestCase):
         resultsdb = ResultsDBApi("", "kernel-3.10.0-632.el7", "1", "https://url.corp.redhat.com/resultdb2", "")
         self.assertEqual(len(resultsdb.get_resultsdb_data()), 200)
 
+    # Messagehub testing section
+    def test_env_message_part(self):
+        data = {"old": "OPEN", "new": "FAILED", "attribute": "state"}
+        os.environ['TEST'] = json.dumps(data)
+        output = env_run(SimpleClass('TEST'))
+        self.assertDictEqual(output, data)
+
+    def test_env_message_part_with_newlines(self):
+        data = '{\n"old": \n"OPEN", \n"new": \n"FAILED", \n"attribute": \n"state"}'
+        data_without_newlines = {"old": "OPEN", "new": "FAILED", "attribute": "state"}
+        os.environ['TEST'] = data
+        output = env_run(SimpleClass('TEST'))
+        self.assertDictEqual(output, data_without_newlines)
+    # End of messagehub testing section
 
 if __name__ == '__main__':
     unittest.main()
